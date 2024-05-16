a28197f4818effcc00fa0a9011e7f8223d2e37c5
Egor Zalizny <zaliznyje@gmail.com>
2024-05-16 14:01:21 +0200

    feat: вынес App в отдельный файл

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,7 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 import "./index.scss";
-
-function App() {
-  const [count, setCount] = React.useState(0);
-
-  return (
-    <>
-      <h1>Hello React + Webpack</h1>
-      <h3>Count: {count}</h3>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-    </>
-  );
-}
 
 const root = createRoot(document.getElementById("app"));
 
