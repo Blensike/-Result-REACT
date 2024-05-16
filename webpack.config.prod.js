cfeef7e5af7b1bde5184d35f8d86b1abb22cdd86
Egor Zalizny <zaliznyje@gmail.com>
2024-05-16 18:35:31 +0200

    скачал BundleAnalyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,19 @@
 const { merge } = require("webpack-merge");
 const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const commonConfig = require("./webpack.config.common");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const commonConfig = require("./webpack.config.common"); //ставь на последнем месте
 
 module.exports = merge(commonConfig, {
   mode: "production",
   devtool: "source-map",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+      reportFilename: "report.html",
+    }),
+  ],
   optimization: {
     minimize: true,
     minimizer: [new CssMinimizerWebpackPlugin(), new TerserPlugin()],
